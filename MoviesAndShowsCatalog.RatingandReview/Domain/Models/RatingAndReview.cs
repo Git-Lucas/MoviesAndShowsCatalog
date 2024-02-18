c82f0ebe2c65dc51fc85a4be0542d710278c260c
Git-Lucas <lucas.oliver_2000@hotmail.com>
2024-02-17 21:34:42 -0300

    [MS] - feat: Criado objeto de domínio, mapeado no ORM, e criada migração para criação da tabela no banco de dados

---
+++ MoviesAndShowsCatalog.RatingandReview/Domain/Models/RatingAndReview.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MoviesAndShowsCatalog.RatingAndReview.Domain.Models;
+
+public class RatingAndReview
+{
+    public int Id { get; set; }
+    [Range(0, 5, ErrorMessage = $"The {nameof(Rating)} must be between 0 and 5")]
+    public float Rating { get; set; }
+    public string Review { get; set; } = string.Empty;
+    public int VisualProductionId { get; set; }
+    public VisualProduction? VisualProduction { get; set; }
+}
