78b78e44cf3e00187181775877e1f328d966366e
Git-Lucas <lucas.oliver_2000@hotmail.com>
2024-05-13 16:22:25 -0300

    [MS] - feat: Definidas filas duráveis e não exclusivas para resiliência de mensagens em caso de aplicações consumidoras fora do ar

--- MoviesAndShowsCatalog.RatingAndReview/Infrastructure/RabbitMQ/RabbitMQSubscriber.cs
+++ MoviesAndShowsCatalog.RatingAndReview/Infrastructure/RabbitMQ/RabbitMQSubscriber.cs
@@ -11,7 +11,7 @@ public class RabbitMQSubscriber : BackgroundService
     private readonly IEventProcessor _eventProcessor;
     private readonly IModel _channel;
     private readonly string _exchangeName = "VisualProductionExchange";
-    private readonly string? _queueName;
+    private readonly string _queueName = $"{nameof(RatingAndReview)}Queue";
 
     public RabbitMQSubscriber(ILogger<RabbitMQSubscriber> logger, ConfigRabbitMQ config, IEventProcessor eventProcessor)
     {
@@ -27,16 +27,20 @@ public class RabbitMQSubscriber : BackgroundService
                                      durable: true,
                                      autoDelete: false,
                                      arguments: null);
-            _queueName = _channel.QueueDeclare().QueueName;
 
-            _channel.QueueBind(
-                queue: _queueName,
-                exchange: _exchangeName,
-                routingKey: "Created");
-            _channel.QueueBind(
-                queue: _queueName,
-                exchange: _exchangeName,
-                routingKey: "Deleted");
+            _channel.QueueDeclare(queue: _queueName,
+                                  durable: true,
+                                  exclusive: false,
+                                  autoDelete: false,
+                                  arguments: null);
+
+            _channel.QueueBind(queue: _queueName,
+                               exchange: _exchangeName,
+                               routingKey: "Created");
+
+            _channel.QueueBind(queue: _queueName,
+                               exchange: _exchangeName,
+                               routingKey: "Deleted");
         }
         catch (Exception ex)
         {
@@ -53,13 +57,21 @@ public class RabbitMQSubscriber : BackgroundService
             byte[] body = ea.Body.ToArray();
             string message = Encoding.UTF8.GetString(body);
 
-            _eventProcessor.ProcessAsync(ea.RoutingKey, message);
+            try
+            {
+                _eventProcessor.ProcessAsync(ea.RoutingKey, message);
+
+                _channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Unable to process the message: '{message}'. Error: {ex.Message}");
+            }
         };
 
-        _channel.BasicConsume(
-            queue: _queueName,
-            autoAck: true,
-            consumer: consumer);
+        _channel.BasicConsume(queue: _queueName,
+                              autoAck: false,
+                              consumer: consumer);
 
         return Task.CompletedTask;
     }
--- MoviesAndShowsCatalog.User/Infrastructure/RabbitMQ/RabbitMQSubscriber.cs
+++ MoviesAndShowsCatalog.User/Infrastructure/RabbitMQ/RabbitMQSubscriber.cs
@@ -11,7 +11,7 @@ public class RabbitMQSubscriber : BackgroundService
     private readonly IEventProcessor _eventProcessor;
     private readonly IModel _channel;
     private readonly string _exchangeName = "VisualProductionExchange";
-    private readonly string? _queueName;
+    private readonly string? _queueName = $"{nameof(User)}Queue";
 
     public RabbitMQSubscriber(ILogger<RabbitMQSubscriber> logger, ConfigRabbitMQ config, IEventProcessor eventProcessor)
     {
@@ -27,12 +27,16 @@ public class RabbitMQSubscriber : BackgroundService
                                      durable: true,
                                      autoDelete: false,
                                      arguments: null);
-            _queueName = _channel.QueueDeclare().QueueName;
 
-            _channel.QueueBind(
-                queue: _queueName,
-                exchange: _exchangeName,
-                routingKey: "Created");
+            _channel.QueueDeclare(queue: _queueName,
+                                  durable: true,
+                                  exclusive: false,
+                                  autoDelete: false,
+                                  arguments: null);
+
+            _channel.QueueBind(queue: _queueName,
+                               exchange: _exchangeName,
+                               routingKey: "Created");
         }
         catch (Exception ex)
         {
@@ -49,13 +53,21 @@ public class RabbitMQSubscriber : BackgroundService
             byte[] body = ea.Body.ToArray();
             string message = Encoding.UTF8.GetString(body);
 
-            _eventProcessor.ProcessAsync(ea.RoutingKey, message);
+            try
+            {
+                _eventProcessor.ProcessAsync(ea.RoutingKey, message);
+
+                _channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Unable to process the message: '{message}'. Error: {ex.Message}");
+            }
         };
 
-        _channel.BasicConsume(
-            queue: _queueName,
-            autoAck: true,
-            consumer: consumer);
+        _channel.BasicConsume(queue: _queueName,
+                              autoAck: false,
+                              consumer: consumer);
 
         return Task.CompletedTask;
     }
