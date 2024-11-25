2c990100d4eaf40856d78e8d95ba661e5a2ab87b
Lucas de Oliveira Gonçalves <lucas.oliver_2000@hotmail.com>
2024-11-25 13:25:24 -0300

    feat: Para seguir o padrão dos outros microsserviços e repassar a responsabilidade da maneira de lançar exceções para camada de aplicação, retirado 'try/catch' de Controller 'RatingAndReview'

--- MoviesAndShowsCatalog.RatingAndReview/Web/Controllers/RatingsAndReviewsController.cs
+++ MoviesAndShowsCatalog.RatingAndReview/Web/Controllers/RatingsAndReviewsController.cs
@@ -14,59 +14,31 @@ public class RatingsAndReviewsController : ControllerBase
     [ProducesResponseType(StatusCodes.Status201Created)]
     public async Task<IActionResult> CreateAsync([FromServices] CreateRatingAndReview useCase, [FromBody] CreateRatingAndReviewRequest createRatingAndReviewDTO)
     {
-        try
-        {
-            await useCase.ExecuteAsync(createRatingAndReviewDTO);
-            return Created(string.Empty, string.Empty);
-        }
-        catch (Exception ex)
-        {
-            return BadRequest(ex.Message);
-        }
+        await useCase.ExecuteAsync(createRatingAndReviewDTO);
+        return Created(string.Empty, string.Empty);
     }
 
     [HttpGet("{visualProductionId:int}")]
     [ProducesResponseType(typeof(GetRatingsAndReviewsResponse), StatusCodes.Status200OK)]
     public async Task<IActionResult> GetAllByVisualProductionIdAsync([FromServices] GetRatingsAndReviewsByVisualProductionId useCase, [FromRoute] int visualProductionId)
     {
-        try
-        {
-            GetRatingsAndReviewsResponse ratingAndReviewResponse = await useCase.ExecuteAsync(visualProductionId);
-            return Ok(ratingAndReviewResponse);
-        }
-        catch (Exception ex)
-        {
-            return BadRequest(ex.Message);
-        }
+        GetRatingsAndReviewsResponse ratingAndReviewResponse = await useCase.ExecuteAsync(visualProductionId);
+        return Ok(ratingAndReviewResponse);
     }
 
     [HttpGet("bestRated")]
     [ProducesResponseType(typeof(GetRatingsAndReviewsResponse), StatusCodes.Status200OK)]
     public IActionResult GetBestRated([FromServices] GetBestRatedVisualProduction useCase)
     {
-        try
-        {
-            GetRatingsAndReviewsResponse bestRatedVisualProduction = useCase.Execute();
-            return Ok(bestRatedVisualProduction);
-        }
-        catch (Exception ex)
-        {
-            return BadRequest(ex.Message);
-        }
+        GetRatingsAndReviewsResponse bestRatedVisualProduction = useCase.Execute();
+        return Ok(bestRatedVisualProduction);
     }
 
     [HttpGet("worstRated")]
     [ProducesResponseType(typeof(GetRatingsAndReviewsResponse), StatusCodes.Status200OK)]
     public IActionResult GetWorstRated([FromServices] GetWorstRatedVisualProduction useCase)
     {
-        try
-        {
-            GetRatingsAndReviewsResponse worstRatedVisualProduction = useCase.Execute();
-            return Ok(worstRatedVisualProduction);
-        }
-        catch (Exception ex)
-        {
-            return BadRequest(ex.Message);
-        }
+        GetRatingsAndReviewsResponse worstRatedVisualProduction = useCase.Execute();
+        return Ok(worstRatedVisualProduction);
     }
 }
